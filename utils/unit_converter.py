b9e14320896eaa41239ecfe8052809ec093bdca7
Bonismo <bonismo@amazon.com>
2025-07-16 21:46:23 +0800

    commit

--- utils/unit_converter.py
+++ utils/unit_converter.py
@@ -5,7 +5,13 @@
 使用十进制单位 (KB, MB, GB) 进行网络相关计算
 """
 
+import sys
+import os
 from typing import Union, Dict, List
+
+# 添加项目根目录到Python路径
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from utils.unified_logger import get_logger
 
 logger = get_logger(__name__)
